0f645180513515638a19430542413cf5b3d2e1ee
Arda Coskun <inteera@protonmail.com>
2025-03-12 13:16:09 +0300

    739

--- Solutions/739.daily-temperatures.java
+++ Solutions/739.daily-temperatures.java
@@ -6,46 +6,17 @@
 
 // @lc code=start
 class Solution {
-    public int[] dailyTemperatures(int[] temperatures) {
-        if(temperatures.length == 1){
-            return new int[]{0};
-        }
-
-        int[] answer = new int[temperatures.length];
-
+    public int[] dailyTemperatures(int[] temps) {
+        int[] results = new int[temps.length];
         Stack<Integer> stack = new Stack<>();
-        int i = 1, j = 1, temp = 1;
-        for(int element : temperatures){
-            if(!stack.empty()){
-                if(element <= stack.peek()){
-                    i++;
-                    stack.push(element);
-                    continue;
-                }
-                while(!stack.empty() && element > stack.peek()){
-                    i--;
-                    answer[i] = temp;
-                    j++;
-                    temp++;
-                    stack.pop();
-                }
-                if(!stack.empty()){
-                    temp = j;
-                }
-                else{
-                    temp = 1;
-                }
-                stack.push(element);
-                j = 1;
-                i += 2;
+        for (int i = 0; i < temps.length; i++) {
+            while (!stack.isEmpty() && temps[stack.peek()] < temps[i]) {
+                results[stack.peek()] = i - stack.pop();
             }
-            else{
-                stack.push(element);
-            }
-            
+            stack.push(i);
         }
 
-        return answer;
+        return results;
     }
 }
 // @lc code=end
